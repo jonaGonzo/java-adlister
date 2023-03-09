541b0503a1d5749c1a2d73f3382978af2d549e49
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2023-03-09 11:31:15 -0600

    servlets exercise

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/count")
+public class CounterServlet extends HttpServlet {
+
+    public static int count = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String reset = request.getParameter("reset");
+        if (reset != null) {
+            count = 0;
+        }
+        response.getWriter().println(count);
+        count++;
+    }
+
+}
